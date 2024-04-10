ca7160c988078ca60b93d7d150f554624691dcc0
Amirhosseinchch <chalehca@tcd,ie>
2024-04-10 12:30:58 +0100

    added feature for considering content words only or not when calculating semantic dissimilarity

--- SemanticDiss.py
+++ SemanticDiss.py
@@ -7,14 +7,15 @@ Created on Tue Apr  9 16:40:13 2024
 
 import numpy as np
 import pandas as pd
-import glob
-from gensim.test.utils import datapath, get_tmpfile
+from gensim.test.utils import get_tmpfile
 from gensim.models import KeyedVectors
-import scipy
-
+from nltk.corpus import stopwords 
 #%%
-path = r'E:\Bonnie\Bonnie\Autism_Data\Stimuli_Target\Cond1\Cond_1_01.csv'
-def SemDiss(path,sr,metric='Corr'):
+
+def SemDiss(path,sr,metric='Corr',functionWords=False):
+    stop = set(stopwords.words('english'))
+    if functionWords==True: ##### if we also want to calculate sementic diss for function words
+        stop={}
     tmp_file = get_tmpfile(r"E:\mb_variation\mb_variation\W2VModel\vectors.bin")
     Semanticmodel =  KeyedVectors.load_word2vec_format(tmp_file,binary=True,limit=299887)
     diss_all=[]
@@ -24,20 +25,21 @@ def SemDiss(path,sr,metric='Corr'):
     
     length = int(np.round(df.iloc[-1]['endTime']*sr))
     print(length)
-    SemanticVector = np.zeros((length,1))
+
     for i,c in df.iterrows():
         # if i==2:
         #     break
         if i==0: ### begining of sentence
-            df.at[i,'SemanticDiss']=1
-            diss = 1
+            df.at[i,'SemanticDiss']=0
+            diss = 0
             diss_all.append(diss)
             
             continue
         
-        if c['word'].lower() not in Semanticmodel.index_to_key: ### skipping words that aree not in dict
+        if c['word'].lower() not in Semanticmodel.index_to_key or \
+            c['word'].lower() in stop: ### skipping words that aree not in dict
             
-            print('word not in w2v dictionary')
+            print('word not in w2v dictionary or is function word')
                 
             continue
         
@@ -48,21 +50,25 @@ def SemDiss(path,sr,metric='Corr'):
         df_context = df.iloc[:i]
         
         context_words = [w.lower() for w in df_context['word']] ##### all words in previous context
-        print(fr'Context words are :{context_words}')
+        # print(fr'Context words are :{context_words}')
         contextVectors = [Semanticmodel.get_vector(w).reshape(1,-1) for w in context_words
-                          if w in Semanticmodel.index_to_key]
-        
+                          if (w in Semanticmodel.index_to_key) 
+                          and (w not in stop)]
+        if len(contextVectors)==0:
+            print('context words do not exist, skipping')
+            df.at[i,'SemanticDiss']=1
+            continue
         contextVectors = np.array(contextVectors)
         contextVectorsMean = np.mean(contextVectors,axis=0)
         
         WordVector = Semanticmodel.get_vector(token)
     
         if metric=='Corr':
-            print(token,df_context['word'])
+            # print(token,df_context['word'])
             coef = np.corrcoef(WordVector,contextVectorsMean)[0,1]
             
             diss=1-coef
-            print(diss,i)
+            # print(diss,i)
             df.at[i,'SemanticDiss']=diss
             diss_all.append(diss)
     return df
